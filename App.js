6fe66c19e8203a8ee1af6348557710de3556e20e
olasubomi olawepo <olasubomiolawepo@gmail.com>
2023-11-05 01:36:11 -0500

    UI Update

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 
 import { StyleSheet, Text, View } from 'react-native';
-import FlightCancellationScreen from './UI/CancellationScreen';
-import Home from './UI/Home'
+import HomeScreen from './UI/Home';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export default function App() {
   return (
-    <FlightCancellationScreen/>
-    // <Home/>
+    <HomeScreen/>
+
+
 )
 }
 
--- UI/Home.js
+++ UI/Home.js
@@ -1,100 +1,82 @@
 
 import React from 'react';
-import { TouchableOpacity, Dimensions, View, Text, StyleSheet, ImageBackground, Image} from 'react-native';
+import { TouchableOpacity, Dimensions, View, Text, StyleSheet, ImageBackground, Image, ScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import BottomContainer from './PullUpScreen';
+import BottomNavigation from './bottomnav';
 
 
 const word = "If you are traveling November 5, 2023, please wait until \n23 hours before scheduled departure to check-in."
-const word2 = "Earn 50,000 bonus miles \nPlus, first checked bag is free on \ndomestic American Airlines itineraries \nTerms apply.\nLearn more ->"
+const word2 = "Earn 50,000 bonus miles \nPlus, first checked bag is free on domestic American Airlines itineraries \n\nTerms apply.\n \nLearn more ->"
 const screenWidth = Dimensions.get('window').width;
-const shiftPercentage = 80;
+const shiftPercentage = 59.5;
 
-const Home = () => {
+const HomeScreen = () => {
 
     return (
-    <ImageBackground
-      source={require('../assets/american_bg.jpg')}
-      style={styles.backgroundImage}
-    >
-        <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>American</Text>
-            <Image
-                source={require('../assets/american_logo1.png')}
-                style={styles.logo}
-            />
-            <TouchableOpacity style={styles.icons} >
-            <Icon name="comments" size={24} color="#FFFFFF" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.icons} >
-            <Icon name="envelope" size={24} color="#FFFFFF" />
-            </TouchableOpacity>        
-        </View>
+        <><ScrollView>
+            <ImageBackground
+                source={require('../assets/american_bg.jpg')}
+                style={styles.backgroundImage}
+            >
+                <View style={styles.headerContainer}>
+                    <Text style={styles.headerText}>American</Text>
+                    <Image
+                        source={require('../assets/american_logo1.png')}
+                        style={styles.logo} />
+                    <TouchableOpacity style={styles.icons}>
+                        <Icon name="comments" size={24} color="#FFFFFF" />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.icons}>
+                        <Icon name="envelope" size={24} color="#FFFFFF" />
+                    </TouchableOpacity>
+                </View>
 
-        <TouchableOpacity style={styles.userInfoContainer}>
-        <Text style={styles.userName}>Opeyeoluwa Olanipekun</Text>
-        <Text style={styles.memberStatus}>AAdvantage member</Text>
-        <Text style={styles.awardMiles}>Available Award miles : 40,000</Text>
-        </TouchableOpacity>
+                <TouchableOpacity style={styles.userInfoContainer}>
+                    <Text style={styles.userName}>Opeyeoluwa Olanipekun</Text>
+                    <Text style={styles.memberStatus}>AAdvantage member</Text>
+                    <Text style={styles.awardMiles}>Available Award miles : 45,000</Text>
+                </TouchableOpacity>
 
-        <View style={styles.daylightSavings}>
-        <View style = {{backgroundColor:"#ff4d01", padding: 10}}>
-        <Text style={styles.daylightHeaderText}>Daylight Savings Time is Ending</Text>
-        </View>
-        <Text style={styles.daylightText}>{word}</Text>
-        </View>
+                <View style={styles.daylightSavings}>
+                    <View style={{
+                        backgroundColor: "#ff4d01", padding: 10, width: 353, borderTopLeftRadius: 10, borderTopRightRadius: 10
+                    }}>
+                        <Text style={styles.daylightHeaderText}>Daylight Savings Time is Ending</Text>
+                    </View>
+                    <Text style={styles.daylightText}>{word}</Text>
+                </View>
 
-        <View style={styles.planeHeader}>
-        <Text style={styles.planeInfo}>AA 4853</Text>
-        <View style={{backgroundColor:'green', width: 80, marginLeft: (screenWidth * shiftPercentage) / 100, }}>
-        <Text style={{margin: 10, color: 'white'}}>ON TIME</Text>
-        </View>
-        <Text style={styles.planetext}>Departs Sunday, 4:52 PM</Text>
-        <Text style={styles.date}>SUNDAY, NOVEMBER 5, 2023. LUXEVU</Text>
-        </View>
+                <View style={styles.planeHeader}>
+                    <Text style={styles.planeInfo}>AA 4853</Text>
+                    <View style={{ backgroundColor: 'red', width: 100, height: 30, marginLeft: (screenWidth * shiftPercentage) / 100, marginTop: -30, marginBottom: 15 }}>
+                        <Text style={{ margin: 7, color: 'white', textAlign: 'center', fontSize: 12, fontWeight: '600' }}>DELAYED</Text>
+                    </View>
+                    <Text style={styles.planetext}>DFW -> HSV</Text>
+                    <View style={styles.line}></View>
+                    <Text style={styles.planetext}>Departs Sunday, 4:52 PM</Text>
+                    <Text style={styles.date}>SUNDAY, NOVEMBER 5, 2023. LUXEVU</Text>
+                </View>
 
-        <View style={{margin: 20, flex: 1, backgroundColor:'rgba(255, 255, 255, 0.5)', height:400, alignItems: 'center', justifyContent:'center', padding: 20, borderRadius: 10}}>
-        <Ionicons name="airplane-outline" size={54} color="black" style = {{marginTop: 20, alignContent: 'center'}} />
-        <View style={{ marginTop: 10,marginLeft: 0,flexDirection: "row",flexWrap: "wrap",flexBasis: "auto",flexGrow: 1, alignContent:'flex-start', justifyContent: 'flex-start'}}>
-            <Text style={styles.screen2_text}> Departs {'\n'} DFW {'\n'} 7:00am </Text>
-            <View>
-            <Ionicons name="alarm-outline" size={24} color="black" style={{marginLeft: 19, marginTop: 10}}/>
-                <Text style={{fontSize: 15, textAlign: 'center', color: 'black', marginRight: 2}}> {'\n'} 2h 50min</Text>
-            </View>
-            <Text style={styles.screen2_text}>Arrives {'\n'} HSV {'\n'} 9:50am</Text>
-        </View> 
-        <View style={{ marginTop: 10,marginLeft: 15,flexDirection: "row",flexWrap: "wrap",flexBasis: "auto",flexGrow: 1, alignContent:'flex-start', justifyContent: 'flex-start'}}>
-            <Text style={styles.gate_text}> GATE {'\n'} A </Text>
-            <Text style={styles.gate_text}> Terminal {'\n'} A 20 </Text>
-            <Text style={styles.gate_text}> GATE {'\n'} C</Text>
-            <Text style={styles.gate_text}> Terminal {'\n'} C 20 </Text>
-        </View> 
-        </View>
-        <TouchableOpacity style = {{backgroundColor: '#0096fe', margin: 20, marginTop: 0, padding: 10}}>
-        <Text style={{color: 'white', fontSize: 20, textAlign: 'center'}}> CHECK IN </Text>
-        </TouchableOpacity>
+                <View style={styles.promotionContainer}>
+                    <Text style={styles.promotionText}>{word2}</Text>
+                    {/* <Image
+                        source={require('../assets/card.png')}
+                        style={styles.creditCard} /> */}
+                </View>
+                {/* <BottomSwipe/> */}
+            </ImageBackground>
+        </ScrollView><BottomNavigation /></>
 
-        <View style={styles.promotionContainer}>
-        <Text style={styles.promotionText}>{word2}</Text>
-            <Image 
-            source={require('../assets/card.png')}
-            style={styles.creditCard}/>
-        </View>
-        
-
-        <BottomContainer/>
-
-    </ImageBackground>
   );
 };
+
 
 const styles = StyleSheet.create({
     creditCard: {
         padding: 10,
-        width: 150,
+        width: 100,
         height: 100,
-        margin: 5,
+        margin: 0,
         resizeMode: "contain",
     },
     promotionContainer: {
@@ -104,14 +86,15 @@ const styles = StyleSheet.create({
         margin: 20,
     },
     promotionText: {
-        fontSize: 15,
+        fontSize: 14,
         color: "white",
-        paddingRight: 100,
+        paddingRight: 0,
     },
     planeHeader: {
         backgroundColor: 'white',
         padding: 20,
-        margin: 20
+        margin: 20,
+        borderRadius:10
     },
     date: {
         color: 'blue',
@@ -120,28 +103,20 @@ const styles = StyleSheet.create({
         fontSize: 18
     },
     planetext: {
-        fontSize: 28
-    },
-    gate_text:{
-        color: 'black',
-        fontSize: 14,
-        fontWeight: 'bold',
-        padding: 10,
-        textAlign: 'center',
-        alignContent: 'center',
-        marginRight: 3
-    },
-    screen2_text:{
-        color: 'black',
         fontSize: 22,
-        fontWeight: 'bold',
-        padding: 10,
-        textAlign: 'center'
-      },
+    },
     backgroundImage: {
         flex: 1,
         width: '100%',
         height: '100%',
+    },
+    line: {
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        marginVertical: 10,
+    
+        marginLeft: -20, // Add this line
+        marginRight: -20,
     },
     headerContainer: {
         flexDirection: 'row',
@@ -193,16 +168,17 @@ const styles = StyleSheet.create({
         backgroundColor: "#FFFFFF",
         margin: 20,
         alignItems: 'left',
+        borderRadius: 10,
         padding: 0,
     },
     daylightHeaderText: {
-        fontSize: 24,
+        fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#FFFFFF',
     },
     daylightText: {
-        fontSize: 18,
+        fontSize: 13,
         padding: 10,
         marginVertical: 10,
         color: '#000000',
@@ -233,4 +209,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default Home;
+export default HomeScreen;
